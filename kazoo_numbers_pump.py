167a84ac6f101f2546bc535d480b72eac88e0ad2
Kirill Sysoev <tech@onnet.info>
2013-04-23 18:38:55 +0000

    Time of db process added

--- kazoo_numbers_pump.py
+++ kazoo_numbers_pump.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
 
 ##
-## An attempt to get numbers into cache
+## An attempt to get numbers into cache (but it seems it doesnt helps us :) )
+##
+## According to wiki: https://2600hz.atlassian.net/wiki/display/docs/Stepswitch+SUP+Commands
+##
+## sup stepswitch_maintenance lookup_number NUMBER - 
+##
+## When provided with a number Stepswitch will return the known parameters of that number. 
+## These are drawn from the local cache if present or looked up and cached if not. 
+## This provides insight into what account a number is associated with for inbound calls 
+## as well as if outbound calls to this number will stay on-net.
 ##
 
 ## yum install python-pip
@@ -18,7 +27,7 @@ def pump_the_number(number):
     sup_cmd = "/opt/kazoo/utils/sup/sup stepswitch_maintenance lookup_number %s" %(number)
     print sup_cmd
     os.system(sup_cmd)
-    time.sleep(2)
+    time.sleep(7)
 
 if __name__ == '__main__':
 
@@ -40,7 +49,7 @@ if __name__ == '__main__':
         server = couchdb.Server(my_url)
         for db in server:
             if db.startswith("numbers/"):
-                print "\n Found database %s" % db
+                print "\n %s Found database %s" % (time.asctime( time.localtime(time.time()) ), db)
                 process_db(db)
     else:
         print "Get help with -h option"
