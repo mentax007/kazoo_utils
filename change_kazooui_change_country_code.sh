58e907ff4b4fccd91b58189e16489471c8490c99
root <root@kz527.onnet.su>
2013-11-27 15:39:20 +0400

    add change_country_code

---
+++ change_kazooui_change_country_code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Changing country code in Kazoo UI.."
+
+sed -i "s/+?1?/+?7?/g" /var/www/html/kazoo-ui/whapps/numbers/numbers_manager/numbers_manager.js
+sed -i "s/'+1'/'+7'/g" /var/www/html/kazoo-ui/whapps/numbers/numbers_manager/numbers_manager.js
+
+sed -i "s/+?1?/+?7?/g" /var/www/html/kazoo-ui/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+sed -i "s/'+1'/'+7'/g" /var/www/html/kazoo-ui/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+
