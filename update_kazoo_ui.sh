587dc30ff505e2fd5031ae3850a622c8a9827b91
Kirill Sysoev <kirill.sysoev@gmail.com>
2013-11-28 00:07:52 +0400

    U

--- update_kazoo_ui.sh
+++ update_kazoo_ui.sh
@@ -14,10 +14,10 @@ wget https://github.com/onnet/kazoo_utils/raw/master/tmp/config.js -O /var/www/h
 
 echo "Changing country code in Kazoo UI.."
 
-sed -i "s/+?1?/+?7?/g" /var/www/html/kazoo-ui/whapps/numbers/numbers_manager/numbers_manager.js
-sed -i "s/'+1'/'+7'/g" /var/www/html/kazoo-ui/whapps/numbers/numbers_manager/numbers_manager.js
+sed -i "s/+?1?/+?7?/g" /var/www/html/v3/whapps/numbers/numbers_manager/numbers_manager.js
+sed -i "s/'+1'/'+7'/g" /var/www/html/v3/whapps/numbers/numbers_manager/numbers_manager.js
 
-sed -i "s/+?1?/+?7?/g" /var/www/html/kazoo-ui/whapps/pbxs/pbxs_manager/pbxs_manager.js 
-sed -i "s/'+1'/'+7'/g" /var/www/html/kazoo-ui/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+sed -i "s/+?1?/+?7?/g" /var/www/html/v3/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+sed -i "s/'+1'/'+7'/g" /var/www/html/v3/whapps/pbxs/pbxs_manager/pbxs_manager.js 
 
 # Check if "/var/www/html/kazoo-ui/whapps/voip/resource/resource.js" also needed
