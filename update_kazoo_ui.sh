9710b90dcf1f53d0b07cdee06fc177c6f5b1ba06
Kirill Sysoev <kirill.sysoev@gmail.com>
2013-11-28 23:19:21 +0400

    Some elements removal from KAZOO_UI added

--- update_kazoo_ui.sh
+++ update_kazoo_ui.sh
@@ -21,3 +21,15 @@ sed -i "s/+?1?/+?7?/g" /var/www/html/v3/whapps/pbxs/pbxs_manager/pbxs_manager.js
 sed -i "s/'+1'/'+7'/g" /var/www/html/v3/whapps/pbxs/pbxs_manager/pbxs_manager.js 
 
 # Check if "/var/www/html/kazoo-ui/whapps/voip/resource/resource.js" also needed
+
+echo "Removing unused elements..."
+
+#########  Get rid of Credit widget  ###########################################
+sed -i "/'credits': false/d" /var/www/html/v3/whapps/myaccount/myaccount.js
+sed -i "s/'statistics': false,/'statistics': false/g" /var/www/html/v3/whapps/myaccount/myaccount.js
+######### Get rid of Carrier menu in PBX Advanced settings ########
+sed -i "/'resource': false,/d" /var/www/html/v3/whapps/voip/voip.js
+#########  Get rid some staff in myaccount  ####################################
+sed -i "/'billing': false,/d" /var/www/html/v3/whapps/myaccount/myaccount.js 
+sed -i "/'report': false,/d" /var/www/html/v3/whapps/myaccount/myaccount.js 
+################################################################################
