2e10b849e7016eba81e0048f69d6f3dd1269d87f
Kirill Sysoev <kirill.sysoev@gmail.com>
2013-11-29 09:26:21 +0400

    3

--- update_kazoo_ui.sh
+++ update_kazoo_ui.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-###############################################################
-######  Pice of automatition to upgrade KAZOO_UI easier  ######
-###############################################################
+########################################################################
+######  A little pice of automatition to upgrade KAZOO_UI easier  ######
+######  Written by Kirill Sysoev                                  ######
+######             kirill.sysoev@gmail.com                        ######
+########################################################################
+
+#########################################
+######  Change your settings here  ######
+#########################################
 
 KAZOO_UI_DIR="/var/www/html/v3"
 CROSSBAR_SERVER="94.125.5.27"
+COUNTRY_CODE="7"
+
+#########################################
 
 echo "Deleting previous installation"
 
@@ -15,35 +24,75 @@ echo "Retrieving KAZOO_UI master"
 
 git clone https://github.com/2600hz/kazoo_ui.git $KAZOO_UI_DIR
 
+################################
+######  Config.js tweaks #######
+################################
+
 echo "Changing config.js"
 
 sed -i "s/api.2600hz.com/$CROSSBAR_SERVER/g" $KAZOO_UI_DIR/config/config.js
+
+###########################################################
+###### Change your preferred options here by analogy ######
+###########################################################
+
 sed -i "s/hide_registration: false/hide_registration: true/g" $KAZOO_UI_DIR/config/config.js
 sed -i "s/company_name: '2600hz'/company_name: 'OnNet'/g" $KAZOO_UI_DIR/config/config.js
 
+#############################################################
+###### Change country code in number_manager's regexps ######
+#############################################################
+
 echo "Changing country code in Kazoo UI.."
 
-sed -i "s/+?1?/+?7?/g" $KAZOO_UI_DIR/whapps/numbers/numbers_manager/numbers_manager.js
-sed -i "s/'+1'/'+7'/g" $KAZOO_UI_DIR/whapps/numbers/numbers_manager/numbers_manager.js
+sed -i "s/+?1?/+?$COUNTRY_CODE?/g" $KAZOO_UI_DIR/whapps/numbers/numbers_manager/numbers_manager.js
+sed -i "s/'+1'/'+$COUNTRY_CODE'/g" $KAZOO_UI_DIR/whapps/numbers/numbers_manager/numbers_manager.js
 
-sed -i "s/+?1?/+?7?/g" $KAZOO_UI_DIR/whapps/pbxs/pbxs_manager/pbxs_manager.js 
-sed -i "s/'+1'/'+7'/g" $KAZOO_UI_DIR/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+sed -i "s/+?1?/+?$COUNTRY_CODE?/g" $KAZOO_UI_DIR/whapps/pbxs/pbxs_manager/pbxs_manager.js 
+sed -i "s/'+1'/'+$COUNTRY_CODE'/g" $KAZOO_UI_DIR/whapps/pbxs/pbxs_manager/pbxs_manager.js 
 
 # Check if "/var/www/html/kazoo-ui/whapps/voip/resource/resource.js" also needed
 
+####################################################i###################################
+###### Remove unused at KAZOO_UI elements. For example:                           ######
+###### - we do not use built in billing;                                          ######
+###### - we do not provide an ability to route calls to third party carriers yet; ######
+###### - we do not provide subaccounts yet.                                       ######
+########################################################################################
+
 echo "Removing unused elements..."
 
-#########  Get rid of Credit widget  ###########################################
+#########  Get rid of Credit widget  ###################################################
+
 sed -i "/'credits': false/d" $KAZOO_UI_DIR/whapps/myaccount/myaccount.js
 sed -i "s/'statistics': false,/'statistics': false/g" $KAZOO_UI_DIR/whapps/myaccount/myaccount.js
+
 ######### Get rid of Carrier menu in PBX Advanced settings ########
+
 sed -i "/'resource': false,/d" $KAZOO_UI_DIR/whapps/voip/voip.js
-#########  Get rid of some staff in "myaccount"  ####################################
+
+#########  Get rid of some staff in "myaccount"  #######################################
+
 sed -i "/'billing': false,/d" $KAZOO_UI_DIR/whapps/myaccount/myaccount.js 
 sed -i "/'report': false,/d" $KAZOO_UI_DIR/whapps/myaccount/myaccount.js 
-#########  Get rid of Emergency Fields  ####################################
+
+#########  Get rid of Emergency Fields as we do not use them  ##########################
 ## Account
+
 sed -i "/_emergency/d" $KAZOO_UI_DIR/whapps/voip/account/account.js 
 sed -i "/form_data.caller_id.emergency.number/d" $KAZOO_UI_DIR/whapps/voip/account/account.js 
 sed -i "/mergency/d" $KAZOO_UI_DIR/whapps/voip/account/tmpl/edit.html
-################################################################################
+
+## User
+
+sed -i "/_emergency/d" $KAZOO_UI_DIR/whapps/voip/user/user.js
+sed -i "/form_data.caller_id.emergency.number/d" $KAZOO_UI_DIR/whapps/voip/user/user.js 
+sed -i "/mergency/d" $KAZOO_UI_DIR/whapps/voip/user/tmpl/edit.html
+
+## Device
+
+sed -i "/_emergency/d" $KAZOO_UI_DIR/whapps/voip/device/device.js 
+sed -i "/form_data.caller_id.emergency.number/d" $KAZOO_UI_DIR/whapps/voip/device/device.js 
+sed -i "/mergency/d" $KAZOO_UI_DIR/whapps/voip/device/tmpl/edit.html
+
+########################################################################################
