4ac1ca0b491a1c2405378c653f43d35dc1f86a94
Kirill Sysoev <kirill.sysoev@gmail.com>
2015-01-19 17:59:03 +0300

    update

--- kazooui_tweaks/update_kazoo_ui_new.sh
+++ kazooui_tweaks/update_kazoo_ui_new.sh
@@ -25,19 +25,9 @@ echo "Deleting previous installation"
 
 rm -rf $KAZOO_UI_DIR
 
-echo "Retrieving KAZOO_UI master"
+echo "Retrieving KAZOO_UI"
 
 git clone -b v3.18 https://github.com/2600hz/kazoo_ui.git $KAZOO_UI_DIR
-
-echo "`date '+%Y-%m-%d'`" > $KAZOO_UI_DIR/VERSION
-
-wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/b9e5f3cbe4d9f320027bc8d048c2b0a4d172895a/lib/images/OnNet36x36.png -O $KAZOO_UI_DIR/img/wsLogo.png
-
-wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/master/lib/images/logo_for_kazoo.png -O $KAZOO_UI_DIR/config/images/logo.png
-
-wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/master/lib/images/big_onnet_img_for_kazoo.png -O $KAZOO_UI_DIR/config/images/onnet.png
-
-wget --no-check-certificate https://raw.github.com/onnet/kazoo_utils/master/kazooui_tweaks/files/left_welcome.html -O $KAZOO_UI_DIR/config/tmpl/left_welcome.html
 
 ################################
 ######  Config.js tweaks #######
@@ -48,13 +38,6 @@ echo "Changing config.js"
 sed -i "s/api.2600hz.com/$CROSSBAR_SERVER/g" $KAZOO_UI_DIR/config/config.js
 sed -i "s/10.26.0.41/$CROSSBAR_SERVER/g" $KAZOO_UI_DIR/config/config.js
 sed -i "s/api.zswitch.net/$CROSSBAR_SERVER/g" $KAZOO_UI_DIR/config/config.js
-
-###########################################################
-###### Change your preferred options here by analogy ######
-###########################################################
-
-sed -i "s/hide_registration: false/hide_registration: true/g" $KAZOO_UI_DIR/config/config.js
-sed -i "s/company_name: '2600hz'/company_name: 'OnNet - Virtual Office'/g" $KAZOO_UI_DIR/config/config.js
 
 ################################################################################
 ###### Change default infite_format in pbx_manager to 'e164_without_plus' ######
@@ -93,6 +76,19 @@ ln -s /var/www/anti_fraud_checker.php /var/www/html/anti_fraud_checker.php
 ################################################################################################################################################
 ########################################################    For public UI   ####################################################################
 ################################################################################################################################################
+
+echo "`date '+%Y-%m-%d'`" > $KAZOO_UI_DIR/VERSION
+wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/b9e5f3cbe4d9f320027bc8d048c2b0a4d172895a/lib/images/OnNet36x36.png -O $KAZOO_UI_DIR/img/wsLogo.png
+wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/master/lib/images/logo_for_kazoo.png -O $KAZOO_UI_DIR/config/images/logo.png
+wget --no-check-certificate https://raw.github.com/onnet/mod_zonnet/master/lib/images/big_onnet_img_for_kazoo.png -O $KAZOO_UI_DIR/config/images/onnet.png
+wget --no-check-certificate https://raw.github.com/onnet/kazoo_utils/master/kazooui_tweaks/files/left_welcome.html -O $KAZOO_UI_DIR/config/tmpl/left_welcome.html
+
+###########################################################
+###### Change your preferred options here by analogy ######
+###########################################################
+
+sed -i "s/hide_registration: false/hide_registration: true/g" $KAZOO_UI_DIR/config/config.js
+sed -i "s/company_name: '2600hz'/company_name: 'OnNet - Virtual Office'/g" $KAZOO_UI_DIR/config/config.js
 
 ########################################################################################
 ###### Remove unused at KAZOO_UI elements. For example:                           ######
