43748a00f5e27012b7fb83c5eec3534f5dbbfcfa
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-12-07 15:10:27 +0000

    update

---
+++ flush_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sup kz_cache flush
+sup kazoo_data_maintenance flush
+sup kapps_config flush
+sup kapps_config flush number_manager
+sup ecallmgr_config flush 
+sup stepswitch_maintenance reload_resources
+sup kazoo_services_maintenance refresh
+sup trunkstore_maintenance flush
